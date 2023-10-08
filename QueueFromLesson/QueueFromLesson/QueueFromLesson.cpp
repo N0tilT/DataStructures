b6fc8095e67a5703df0d620345ee2dddc9824254
Timofey Latypov <timtompson3241@gmail.com>
2023-10-08 10:20:41 +0300

    initial
    queue and stack added

---
+++ QueueFromLesson/QueueFromLesson/QueueFromLesson.cpp
@@ -0,0 +1,107 @@
+﻿#include <iostream>
+#include <string.h>
+#include <time.h>
+using namespace std;
+
+class Queue
+{
+	//Очередь
+	int* Data;
+	//Максимальный размер очереди
+	int MaxQueueLength;
+	//Текущий размер очереди
+	int QueueLength;
+public:
+	//Конструктор
+	Queue(int m);
+	//Деструктор
+	~Queue();
+	//Добавление элемента
+	void Enqueue(int c);
+	//Извлечение элемента
+	int Dequeue();
+	//Очистка очереди
+	void Clear();
+	//Проверка существования элементов в очереди
+	bool IsEmpty();
+	//Проверка на переполнение очереди
+	bool IsFull();
+	//Количество элементов в очереди
+	int GetCount();
+	//демонстрация очереди
+	void Show();
+};
+void Queue::Show() {
+	cout << "\n-----------------------------------\n";
+	for (int i = 0; i < QueueLength; i++) {
+		cout << Data[i] << " ";
+	}
+	cout << "\n-----------------------------------\n";
+}
+Queue::~Queue()
+{
+	delete[]Data;
+}
+Queue::Queue(int m)
+{
+	MaxQueueLength = m;
+	Data = new int[MaxQueueLength];
+	QueueLength = 0;
+}
+void Queue::Clear()
+{
+	QueueLength = 0;
+}
+bool Queue::IsEmpty()
+{
+	return QueueLength == 0;
+}
+bool Queue::IsFull()
+{
+	return QueueLength == MaxQueueLength;
+}
+int Queue::GetCount()
+{
+	return QueueLength;
+}
+void Queue::Enqueue(int c)
+{
+	if (!IsFull())
+		Data[QueueLength++] = c;
+}
+int Queue::Dequeue()
+{
+	//Если в очереди есть элементы, то возвращаем тот,
+	//который вошел первым и сдвигаем очередь
+	if (!IsEmpty()) {
+
+		//запомнить первый
+		int temp = Data[0];
+
+		//сдвинуть все элементы
+		for (int i = 1; i < QueueLength; i++)
+			Data[i - 1] = Data[i];
+		QueueLength--;
+
+		//вернуть первый(нулевой)
+		return temp;
+	}
+	else
+		return -1;
+}
+
+
+void main()
+{
+	srand(time(0));
+	//создание очереди
+	Queue queue(25);
+
+	for (int i = 0; i < 5; i++) {
+		queue.Enqueue(rand() % 50);
+	}
+
+	queue.Show();
+	queue.Dequeue();
+	queue.Show();
+}
---
+++ StackFromLesson/StackFromLesson/StackFromLesson.cpp
@@ -0,0 +1,103 @@
+﻿#include <iostream>
+#include <string.h>
+#include <time.h>
+using namespace std;
+
+class Stack
+{
+	//Нижняя и верхняя границы стека
+	enum { EMPTY = -1, FULL = 20 };
+
+	//Массив для хранения данных
+	char st[FULL + 1];
+
+	//индекс определяющий вершину стека
+	int top;
+
+public:
+
+	//Конструктор
+	Stack();
+
+	//Добавление элемента
+	void Push(char c);
+
+	//Извлечение элемента
+	char Pop();
+
+	//Очистка стека
+	void Clear();
+
+	//Проверка существования элементов в стеке
+	bool IsEmpty();
+
+	//Проверка на переполнение стека
+	bool IsFull();
+
+	//Количество элементов в стеке
+	int GetCount();
+};
+
+Stack::Stack()
+{
+	//Изначально стек пуст
+	top = EMPTY;
+}
+
+void Stack::Clear()
+{
+	top = EMPTY;
+}
+
+bool Stack::IsEmpty()
+{
+	return top == EMPTY;
+}
+
+bool Stack::IsFull()
+{
+	return top == FULL;
+}
+
+int Stack::GetCount()
+{
+	return top + 1;
+}
+
+void Stack::Push(char c)
+{
+	if (!IsFull())
+	{
+		st[++top] = c;
+	}
+}
+
+
+char Stack::Pop()
+{
+	if (!IsEmpty())
+		return st[top--];
+	else
+		return 0;
+}
+
+
+void main()
+{
+	srand(time(0));
+	
+	Stack ST;
+	char c;
+	//пока стек не заполнится
+	while (!ST.IsFull()) {
+		c = rand() % 4 + 2;
+		ST.Push(c);
+	}
+
+	ST.Push('a');
+	//пока стек не освободится
+	while (c = ST.Pop()) {
+		cout << c << " ";
+	}
+	cout << "\n\n";
+}
